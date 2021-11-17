8a3f1bfce57eb4bfaccf28231329c427d7b00134
yuripaoloni <yuri.paoloni3@gmail.com>
2021-11-17 18:07:55 +0100

    test: mocking data

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,26 @@
 import { axios } from "api/axios";
+import { getMockBooks } from "mocks/models/Book";
+import { getMockLibraries } from "mocks/models/Library";
 
-export const getData = () => {
-  return axios.get("endpoint");
+export const getLibraries = () => {
+  // return axios.get("/library/list");
+  const data = { data: getMockLibraries(["MockA", "MockB"]) };
+  return data;
+};
+
+export const getBooks = (title) => {
+  const data = {
+    data: getMockBooks([
+      "TitleA",
+      "TitleB",
+      "TitleC",
+      "TitleA",
+      "TitleB",
+      "TitleC",
+      "TitleA",
+      "TitleB",
+      "TitleC",
+    ]),
+  };
+  return data;
 };
