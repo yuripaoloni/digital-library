18df217f46caa36b20f8933374119213addfa384
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2021-11-22 22:39:13 +0100

    feat: implementing landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
-
+import Landing from "pages/Landing";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Landing />
       <Routes>{/* tutte le routes */}</Routes>
       <Footer />
     </>
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,7 +1,127 @@
-import React from "react";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Logo from "../assets/logo.ico";
 
-const Landing = () => {
-  return <div></div>;
-};
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default Landing;
+const theme = createTheme();
+
+export default function Landing() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+            justifyContent: "center",
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Biblioteca digitale
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Portale online per la consultazione dei libri digitalizzati con il
+              sistema BooKeeper. Scegli da un catalogo sempre a tua
+              disposizione.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button
+                style={{
+                  borderRadius: 35,
+                  backgroundColor: "#222C4A",
+                }}
+                variant="contained"
+              >
+                Trova un Libro
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: "56.25%",
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      style={{
+                        color: "#222C4A",
+                      }}
+                      size="small"
+                    >
+                      Read
+                    </Button>
+                    <Button
+                      style={{
+                        color: "#222C4A",
+                      }}
+                      size="small"
+                    >
+                      Detail
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+  );
+}
