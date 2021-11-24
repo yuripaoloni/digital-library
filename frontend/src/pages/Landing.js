306e60d3763576ab3589343bbb1caab3974629da
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2021-11-24 17:33:14 +0100

    test : Landing Page Tests

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
@@ -11,17 +12,16 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Logo from "../assets/logo.ico";
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
-export default function Landing() {
+export default function Landing({ test }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <main>
+      <main data-testid={test}>
         <Box
           sx={{
             bgcolor: "background.paper",
@@ -37,6 +37,7 @@ export default function Landing() {
               align="center"
               color="text.primary"
               gutterBottom
+              data-testid="main_header"
             >
               Biblioteca digitale
             </Typography>
@@ -45,6 +46,7 @@ export default function Landing() {
               align="center"
               color="text.secondary"
               paragraph
+              data-testid="sub_header"
             >
               Portale online per la consultazione dei libri digitalizzati con il
               sistema BooKeeper. Scegli da un catalogo sempre a tua
@@ -62,6 +64,7 @@ export default function Landing() {
                   backgroundColor: "#222C4A",
                 }}
                 variant="contained"
+                data-testid="trova_button"
               >
                 Trova un Libro
               </Button>
@@ -70,7 +73,7 @@ export default function Landing() {
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
-          <Grid container spacing={4}>
+          <Grid container direction="row" spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
@@ -80,12 +83,15 @@ export default function Landing() {
                     flexDirection: "column",
                   }}
                 >
+                  <CardHeader
+                    title={
+                      <Typography variant="h6" data-testid="book_header">
+                        Books title
+                      </Typography>
+                    }
+                  />
                   <CardMedia
                     component="img"
-                    sx={{
-                      // 16:9
-                      pt: "56.25%",
-                    }}
                     image="https://source.unsplash.com/random"
                     alt="random"
                   />
@@ -93,10 +99,7 @@ export default function Landing() {
                     <Typography gutterBottom variant="h5" component="h2">
                       Heading
                     </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
-                    </Typography>
+                    <Typography>Books description</Typography>
                   </CardContent>
                   <CardActions>
                     <Button
