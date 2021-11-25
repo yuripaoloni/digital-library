d2d811152ad0b32847d355f121831bff51b9f3e5
yuripaoloni <yuri.paoloni3@gmail.com>
2021-11-25 15:41:40 +0100

    test: book details page tests draft

---
+++ frontend/src/__tests__/BookDetails.test.js
@@ -0,0 +1,6 @@
+import { render, fireEvent, waitFor, screen } from "utils/testUtils";
+import App from "App";
+
+xtest("should display book details", async () => {});
+
+xtest("should render NoMatch in case of invalid value for books matrix", async () => {});
