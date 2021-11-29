55e6425ab491e26ac0486937ad9eb6fad739a6cb
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2021-11-29 11:27:54 +0100

    feat : login's design implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
-
 import SearchBooks from "pages/SearchBooks";
 import BookDetails from "pages/BookDetails";
 import Landing from "pages/Landing";
 import NoMatch from "pages/NoMatch";
-
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
-
+import SignIn from "pages/Signin";
 import { fetchRandomBooks, fetchLibraries } from "states/booksSlice";
-
 const App = () => {
   const dispatch = useDispatch();
 
@@ -27,6 +24,7 @@ const App = () => {
         <Route path="/" element={<Landing />} />
         <Route exact path="books" element={<SearchBooks />} />
         <Route exact path="books/:page/:index" element={<BookDetails />} />
+        <Route exact path="signin" element={<SignIn />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
       <Footer />
---
+++ frontend/src/pages/Signin.js
@@ -0,0 +1,85 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            style={{ color: "#222C4A" }}
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            style={{ background: "#222C4A" }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2" style={{ color: "#222C4A" }}>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2" style={{ color: "#222C4A" }}>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
