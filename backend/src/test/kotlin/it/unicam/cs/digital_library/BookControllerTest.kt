db2387068898e735da72c4f86ea8f98f10f2e7bb
Andrea De Angelis <andrea.deangelis@studenti.unicam.it>
2021-12-05 12:31:00 +0100

    test: added BookControllerTest and minor improvement

--- backend/src/test/kotlin/it/unicam/cs/digital_library/BookControllerTest.kt
+++ backend/src/test/kotlin/it/unicam/cs/digital_library/BookControllerTest.kt
@@ -11,12 +11,25 @@ import org.springframework.boot.test.context.SpringBootTest
 class BookControllerTest(@Autowired private val bookController: BookController) {
 
     @Test
-    fun searchBookTest(){
+    fun searchBookTest() {
         assert(bookController.searchBook("diritto").isNotEmpty())
+        assert(bookController.searchBook("diritto", listOf(-1)).isEmpty())
     }
 
     @Test
-    fun searchBookTest1(){
-        assert(bookController.searchBook("diritto", listOf(-1)).isEmpty())
+    fun getCoverTest() {
+        val books = bookController.searchBook("Diritto delle obbligazioni - Lezioni di diritto civile")
+        assert(books.isNotEmpty())
+        val book = books.getOrNull(0)?.getOrNull(0)
+        require(book != null)
+        assert(bookController.getCover(book) != null)
     }
+
+    @Test
+    fun getRandomBooksTest() {
+        val randomBooks = bookController.getRandomBooks()
+        assert(randomBooks.isNotEmpty())
+        assert(randomBooks.first().size <= 10)
+    }
+
 }
--- backend/src/test/kotlin/it/unicam/cs/digital_library/UserControllerTest.kt
+++
@@ -1,4 +0,0 @@
-package it.unicam.cs.digital_library
-
-class UserControllerTest {
-}
