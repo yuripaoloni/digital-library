373b2f735226c77d761e1cc6ca9360dded265804
yuripaoloni <yuri.paoloni3@gmail.com>
2021-12-07 17:50:28 +0100

    test: auth action

--- frontend/src/__tests__/states.test.js
+++ frontend/src/__tests__/states.test.js
@@ -1,14 +1,31 @@
-import reducer from "states/booksSlice";
+import booksSlice from "states/booksSlice";
+import authSlice from "states/authSlice";
 
-test("should return the initial state", () => {
-  expect(reducer(undefined, {})).toEqual({
+test("booksSlice: should return the initial state", () => {
+  expect(booksSlice(undefined, {})).toEqual({
     loading: false,
     libraries: [],
     books: [],
+    pageUrl: "",
     error: {
       error: false,
       variant: "error",
       message: "",
     },
+  });
+});
+
+test("authSlice: should return the initial state", () => {
+  expect(authSlice(undefined, {})).toEqual({
+    loading: false,
+    isAuth: false,
+    isRegistered: false,
+    error: {
+      error: false,
+      variant: "error",
+      message: "",
+    },
+    authToken: localStorage.getItem("authToken"),
+    user: {},
   });
 });
