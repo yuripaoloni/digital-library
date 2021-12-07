0654ab100b15615415801302586edd0ee2d6b008
yuripaoloni <yuri.paoloni3@gmail.com>
2021-12-07 17:47:29 +0100

    refactor: modified server url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
 import Axios from "axios";
 
-//? the proxy value in package.json is used to avoid CORS issues
 export const axios = Axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  auth: localStorage.getItem("authToken"),
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 /**
