9296adff5a88fa3fdfc2a0825e4a2eebdf6b3e1a
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2021-12-09 12:29:31 +0100

    feat : implementing text editor
    using mui-rte library

--- frontend/src/pages/BookNotes.js
+++ frontend/src/pages/BookNotes.js
@@ -8,6 +8,10 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { onDeleteNote } from "states/booksSlice";
 import { styled } from "@mui/material/styles";
 import SelectNotes from "components/SelectNotes";
+import MUIRichTextEditor from "mui-rte";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 
 const Img = styled("img")({
   height: 400,
@@ -16,8 +20,49 @@ const Img = styled("img")({
 });
 
 const BookNotes = () => {
+  const editorTheme = createTheme();
+  Object.assign(editorTheme, {
+    overrides: {
+      MUIRichTextEditor: {
+        root: {
+          width: "90%",
+          marginTop: 30,
+          marginLeft: 10,
+          border: "1px solid grey",
+          borderRadius: 4,
+          maxWidth: "100vh",
+          maxHeight: "80vh",
+        },
+        editor: {
+          display: "block",
+          maxHeight: "80vh",
+          maxWidth: "80vh",
+          padding: "0 13px",
+          marginTop: 2,
+          marginBottom: 15,
+        },
+        container: {
+          display: "flex",
+          flexDirection: "column",
+          margin: 0,
+        },
+        toolbar: {
+          display: "block",
+          order: 2,
+          position: "relative",
+        },
+        placeHolder: {
+          position: "relative",
+        },
+        editorContainer: {
+          padding: 13,
+          margin: 0,
+          fontSize: 13,
+        },
+      },
+    },
+  });
   const { page, index, readingPage } = useParams();
-
   const [showDialog, setShowDialog] = useState(false);
   //? has book, id, page, note properties
   const [note, setNote] = useState({
@@ -37,6 +82,10 @@ const BookNotes = () => {
 
   //TODO improve general layout
 
+  const save = (data) => {
+    console.log(data);
+  };
+
   return (
     <PageWrapper>
       <SelectNotes
@@ -45,38 +94,101 @@ const BookNotes = () => {
         readingPage={readingPage}
         setNote={setNote}
       />
-      <Grid container justifyContent="center">
+      <Grid
+        container
+        justifyContent="center"
+        sx={{
+          maxWidth: "100%",
+          alignItems: "baseline",
+          backgroundColor: "red",
+        }}
+      >
         <Grid container rowSpacing={3}>
-          <Grid item xs data-testid="book-details-item">
-            <Typography variant="h4">Note</Typography>
+          <Grid
+            item
+            xs
+            container
+            direction="column"
+            sx={{
+              marginLeft: "20px",
+            }}
+            data-testid="book-details-item"
+          >
+            {/* <Typography variant="h4">Note</Typography>
             <Typography variant="subtitle1" xs={12}>
               Pagina {readingPage}
-            </Typography>
+            </Typography> */}
           </Grid>
-          <Grid item xs>
-            <Button onClick={() => setShowDialog(true)}>Seleziona nota</Button>
-          </Grid>
+          <Grid
+            item
+            xs
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          ></Grid>
         </Grid>
-        <Grid item md={10} sm={11} xs={12}>
+        <Grid item xs={12}>
           <Grid
             container
-            justifyContent={{ sm: "space-between" }}
-            alignItems="center"
-            rowSpacing={{ xs: 3 }}
+            direction={{ xs: "column", sm: "row" }}
+            justifyContent="space-between"
           >
-            <Grid item lg={7} md={8} sm={7} xs={12}>
+            <Grid
+              item
+              sm={5}
+              container
+              direction="column"
+              justifyContent="start"
+              sx={{
+                height: "inherit",
+              }}
+            >
               {/* //TODO inserire editor note 
                 - the content should be controlled with the note state using the note.note property
                 - should have a submit button to create a new note or edit an existing one note (logic will be implemented later)
                 - should have a delete button
               */}
+              <ThemeProvider theme={editorTheme}>
+                <MUIRichTextEditor
+                  label={
+                    <Typography sx={{ marginLeft: "20px" }}>
+                      Start typing...
+                    </Typography>
+                  }
+                  onSave={save}
+                  controls={["bold", "italic", "bulletList", "save", "clear"]}
+                />
+              </ThemeProvider>
+              <Grid
+                container
+                direction="column"
+                sx={{ margin: "50px 0 0 0px" }}
+              ></Grid>
             </Grid>
-            <Grid item md={3} sm={4} xs={12}>
+            <Grid
+              item
+              container={{ xs: true, sm: false }}
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              sm={3}
+              sx={{
+                marginTop: "50px",
+              }}
+            >
               {loading ? (
                 <Skeleton variant="rectangle" height={400} width={280} />
               ) : (
                 <Img src={pageUrl} loading="lazy" />
               )}
+              <Typography variant="subtitle1" xs={12}>
+                Pagina {readingPage}
+              </Typography>
+              <IconButton onClick={() => setShowDialog(true)}>
+                {" "}
+                <LibraryBooksIcon />{" "}
+              </IconButton>
             </Grid>
           </Grid>
         </Grid>
