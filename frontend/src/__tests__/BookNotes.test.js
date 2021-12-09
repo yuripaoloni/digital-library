23357ada3b4b68fc2345e572e86a6a711e156083
yuripaoloni <yuri.paoloni3@gmail.com>
2021-12-09 16:50:52 +0100

    test: book notes test

---
+++ frontend/src/__tests__/BookNotes.test.js
@@ -0,0 +1,89 @@
+import { render, screen } from "utils/testUtils";
+import userEvent from "@testing-library/user-event";
+import App from "App";
+
+test("should open notes page", async () => {
+  render(<App />);
+
+  expect(await screen.findByTestId(/book-item-0/i)).toBeDefined();
+
+  userEvent.click(screen.getByTestId(/read-button-0/i));
+
+  //? after the read button click, should redirect to login page
+  userEvent.type(screen.getByTestId("signin_email_field"), "email@gmail.com");
+  userEvent.type(screen.getByTestId("signin_password_field"), "password123");
+
+  userEvent.click(screen.getByTestId("signin_submit_button"));
+
+  //? after login should redirect to read page
+  expect(await screen.findByTestId(/book-read-item/i)).toBeDefined();
+
+  userEvent.click(screen.getByTestId("ModeIcon"));
+
+  expect(await screen.findByText(/Pagina/i)).toBeDefined();
+
+  userEvent.click(await screen.findByText(/Seleziona nota/i));
+
+  expect(await screen.findAllByText(/MockNote/i)).toBeDefined();
+});
+
+test("should open the notes dialog", async () => {
+  render(<App />);
+
+  userEvent.click(await screen.findByText(/Seleziona nota/i));
+
+  expect(await screen.findAllByText(/MockNote/i)).toBeDefined();
+  // screen.debug(null, Infinity);
+});
+
+test("should create a new note", async () => {
+  render(<App />);
+
+  userEvent.click(await screen.findByText(/Seleziona nota/i));
+
+  userEvent.click(await screen.findByText(/Crea nuova nota/i));
+
+  // screen.debug(null, Infinity);
+
+  //TODO find the editor by testid
+
+  //TODO add content with userEvent
+
+  //TODO click on the save icon to execute the onCreate method
+
+  //TODO check presence of the new note in the dialog
+});
+
+test("should edit an existing note", async () => {
+  render(<App />);
+
+  userEvent.click(await screen.findByText(/Seleziona nota/i));
+
+  userEvent.click(await screen.findByText(/MockNote-0/i));
+
+  //TODO find the editor by testid
+
+  //TODO check presence of the existing note content in the editor
+
+  //TODO modify content with userEvent
+
+  //TODO click on the save icon to execute the onEdit method
+
+  //TODO check presence of the updated note in the dialog
+});
+
+test("should delete an existing note", async () => {
+  render(<App />);
+
+  userEvent.click(await screen.findByText(/Seleziona nota/i));
+
+  userEvent.click(await screen.findByText(/MockNote-0/i));
+
+  //TODO find the editor by testid
+
+  //TODO check presence of the existing note content in the editor
+
+  //TODO click on the delete icon to execute the onDelete method
+
+  //TODO check absence of the deleted note in the dialog
+});
--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -1,6 +1,7 @@
 import { rest } from "msw";
 import { getMockBooks } from "./models/Book";
 import { getMockLibraries } from "./models/Library";
+import { getMockNotes } from "./models/Note";
 
 export const handlers = [
   rest.get("/library/list", (req, res, ctx) =>
@@ -43,5 +44,21 @@ export const handlers = [
         "https://bibliotecadigitale.unicam.it/Library/B.G.5-4/B.G.5-4_0000.JPG"
       )
     )
+  ),
+  rest.post("/note/add", (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(getMockNotes(["CreateMockNote"])))
+  ),
+  rest.post("/note/edit", (req, res, ctx) => {
+    req.body = getMockNotes(["MockNote"]);
+    return res(ctx.status(200), ctx.json(getMockNotes(["EditMockNote"])));
+  }),
+  rest.post("/note/all", (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json(getMockNotes(["MockNote", "MockNote", "MockNote"]))
+    )
+  ),
+  rest.delete("/note/delete", (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(getMockNotes("MockNote")))
   ),
 ];
