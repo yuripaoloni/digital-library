a949e9fcf717ef5078757b2db23fc374d3411685
Luca Fioravanti <luca02.fioravanti@studenti.unicam.it>
2021-12-10 18:45:05 +0100

    fix: token expiration bug solved

--- backend/src/main/kotlin/it/unicam/cs/digital_library/security/jwt/JWTAuthorizationFilter.kt
+++ backend/src/main/kotlin/it/unicam/cs/digital_library/security/jwt/JWTAuthorizationFilter.kt
@@ -14,7 +14,8 @@ import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class JWTAuthorizationFilter(authenticationManager: AuthenticationManager) : BasicAuthenticationFilter(authenticationManager) {
+class JWTAuthorizationFilter(authenticationManager: AuthenticationManager) :
+    BasicAuthenticationFilter(authenticationManager) {
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
         val header = request.getHeader(HEADER_STRING)
 
@@ -27,17 +28,19 @@ class JWTAuthorizationFilter(authenticationManager: AuthenticationManager) : Bas
     }
 
     private fun getAuthentication(request: HttpServletRequest): UsernamePasswordAuthenticationToken? {
-        val token = request.getHeader(HEADER_STRING)
-        if (token != null) {
-            // parse the token.
-            val user = JWT.require(Algorithm.HMAC512(SECRET))
-                .build()
-                .verify(token.replace(TOKEN_PREFIX, ""))
-                .subject
-            return if (user != null) {
-                UsernamePasswordAuthenticationToken(user, null, listOf(SimpleGrantedAuthority("ROLE_USER")))
-            } else null
-        }
-        return null
+        return kotlin.runCatching {
+            val token = request.getHeader(HEADER_STRING)
+            if (token != null) {
+                // parse the token.
+                val user = JWT.require(Algorithm.HMAC512(SECRET))
+                    .build()
+                    .verify(token.replace(TOKEN_PREFIX, ""))
+                    .subject
+                return if (user != null) {
+                    UsernamePasswordAuthenticationToken(user, null, listOf(SimpleGrantedAuthority("ROLE_USER")))
+                } else null
+            }
+            return null
+        }.getOrNull()
     }
 }
