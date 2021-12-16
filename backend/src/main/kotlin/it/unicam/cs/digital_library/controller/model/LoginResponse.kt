83a469491cb453326925e3f1867cbf62ee278f36
Luca Fioravanti <luca02.fioravanti@studenti.unicam.it>
2021-12-16 19:59:16 +0100

    feat: user image added

--- backend/src/main/kotlin/it/unicam/cs/digital_library/controller/model/LoginResponse.kt
+++ backend/src/main/kotlin/it/unicam/cs/digital_library/controller/model/LoginResponse.kt
@@ -1,9 +1,22 @@
 package it.unicam.cs.digital_library.controller.model
 
 import it.unicam.cs.digital_library.model.User
+import org.springframework.util.Base64Utils
 
-class LoginResponse(val email: String, val username: String, val name: String, val surname: String)
+class LoginResponse(
+    val email: String,
+    val username: String,
+    val name: String,
+    val surname: String,
+    val picture: String
+)
 
 fun User.toLoginResponse(): LoginResponse {
-    return LoginResponse(email, username, name, surname)
+    return LoginResponse(
+        email,
+        username,
+        name,
+        surname,
+        Base64Utils.encodeToString(picture)
+    )
 }
