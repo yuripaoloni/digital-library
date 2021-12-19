b4df89b8bc9b88e99a24f384f7c9af878b30985a
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2021-12-19 12:27:48 +0100

    feat: implementing user informations  in PersonalPage

--- frontend/src/pages/PersonalPage.js
+++ frontend/src/pages/PersonalPage.js
@@ -1,5 +1,5 @@
-import { useSelector } from "react-redux";
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -9,12 +9,16 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import { ButtonGroup } from "@mui/material";
+import GroupIcon from "@mui/icons-material/Group";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import BookItem from "components/BookItem";
+import Spinner from "components/Spinner";
+import { onSearchUser } from "states/authSlice";
+import IconButton from "@mui/material/IconButton";
+import PageWrapper from "components/PageWrapper";
 
 /**
  * TODO
@@ -30,48 +34,69 @@ const PersonalPage = () => {
    */
 
   const user = useSelector((state) => state.auth.user);
+  const loading = useSelector((state) => state.auth.userLoading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    Object.keys(user).length === 0 &&
+      dispatch(onSearchUser({ param: localStorage.getItem("username") }));
+  }, [user, dispatch]);
+
+  console.log(user);
+  //TODO spinner o skeleton
+  if (loading) return <Spinner />;
 
   return (
-    <Container container component="main" sx={{ maxWidth: "80vw" }}>
-      <CssBaseline />
-      <Box
-        sx={{
-          backgroundColor: "#F5F5F7",
-          borderRadius: "2vh",
-          boxShadow: 7,
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar
-          alt="Cindy Baker"
-          // src={}
-          style={{
-            margin: "3vh",
-            width: "100px",
-            height: "100px",
-          }}
-        />
+    <PageWrapper>
+      <Container container component="main" sx={{ maxWidth: "80vw" }}>
+        <CssBaseline />
 
-        <Grid container direction="column" spacing={3}>
+        <Box
+          sx={{
+            backgroundColor: "#F5F5F7",
+            borderRadius: "2vh",
+            boxShadow: 7,
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <Grid
+            container
+            direction="column"
+            spacing={3}
+            direction={{ xs: "column", md: "row" }}
+          >
+            {/* <Grid
             container
             direction={{ xs: "column", md: "row" }}
             justifyContent="center"
             alignItems="center"
           >
+            <Avatar
+              alt="Cindy Baker"
+              // src={}
+              style={{
+                margin: "3vh",
+                width: "100px",
+                height: "100px",
+              }}
+            />
+
             <Grid
-              item
-              xs={6}
+              xs={12}
               container
               direction="column"
               justifyContent="center"
               alignItems="center"
             >
               <Typography variant="subtitle1" component="h2">
-                Nome & Congome email username
+                {user.name}
+              </Typography>
+              <Typography variant="subtitle1" component="h2">
+                {user.name}
               </Typography>
             </Grid>
             <Grid
@@ -86,20 +111,51 @@ const PersonalPage = () => {
                 display groups
               </Typography>
             </Grid>
+          </Grid> */}
+            <Grid
+              item
+              xs={12}
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Avatar
+                alt="Cindy Baker"
+                src={`data:image/jpeg;base64,${user.picture}`}
+                style={{
+                  margin: "3vh",
+                  width: "100px",
+                  height: "100px",
+                }}
+              />
+              <Typography variant="h5" component="h2">
+                {user.username}
+              </Typography>
+              <Typography variant="body2" component="h2">
+                {user.email}
+              </Typography>
+              <Typography variant="body2" component="h2">
+                {user.name} & {user.surname}
+              </Typography>
+              <IconButton aria-label="delete" size="large">
+                <GroupIcon />
+              </IconButton>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              books list
+            </Grid>
           </Grid>
-          <Grid
-            item
-            xs={12}
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            books list
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
+        </Box>
+      </Container>
+    </PageWrapper>
   );
 };
 
