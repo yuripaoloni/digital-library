16ce810f6974c29bfc3f187330a5064d4cd83e33
Luca Fioravanti <luca02.fioravanti@studenti.unicam.it>
2022-02-12 15:42:06 +0100

    feat: added password recovery

--- backend/src/main/kotlin/it/unicam/cs/digital_library/email/EmailService.kt
+++ backend/src/main/kotlin/it/unicam/cs/digital_library/email/EmailService.kt
@@ -1,11 +1,13 @@
 package it.unicam.cs.digital_library.email
 
 import it.unicam.cs.digital_library.email.model.client.Email
-import it.unicam.cs.digital_library.email.model.server.SendEmailRequest
-import it.unicam.cs.digital_library.utils.toJson
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
 
@@ -13,28 +15,26 @@ class EmailService {
     private val restTemplate: RestTemplate
 
     companion object {
-        private val FROM = "noreply@digitallibrary.com"
+        private val FROM = "Digial Library <noreply@digitallibrary.com>"
     }
 
     init {
         restTemplate = RestTemplateBuilder()
-            .rootUri("https://api.cloudmailin.com/api/v0.1/71df948c506dea73")
-            .defaultHeader("Authorization", "Bearer bjT9hrxFhJiGoA4M5nxMGfqZ")
-            .defaultHeader("Content-Type", "application/json")
+            .rootUri("https://api.mailgun.net/v3/sandboxb3c8d74ea29d45019c228b708c3a1409.mailgun.org")
+            .defaultHeader("Authorization", "Basic YXBpOjE2ZGUwYWU3MDhkYWI1ZDYyMzk4Mjc5NzhmYTU0NWY0LWQyY2M0OGJjLTg3Mjc2MDhl")
             .build()
     }
 
     fun sendEmail(email: Email): Boolean {
-
-        val request = SendEmailRequest(
-            from = FROM,
-            to = email.to,
-            subject = email.subject,
-            plain = email.plain,
-            html = email.html
-        ).toJson()
-
-        val response = restTemplate.exchange<Any>(url = "/messages", method = HttpMethod.POST, requestEntity = object : HttpEntity<String>(request) {})
+        val response =
+            restTemplate.exchange<Any>(url = "/messages", method = HttpMethod.POST, requestEntity = object : HttpEntity<MultiValueMap<String, String>>(LinkedMultiValueMap<String, String>().apply {
+                add("from", FROM)
+                add("to", email.to)
+                add("subject", email.subject)
+                add("html", email.html)
+            }, HttpHeaders().apply {
+                contentType = MediaType.APPLICATION_FORM_URLENCODED
+            }) {})
 
         return response.statusCode.is2xxSuccessful
     }
--- backend/src/main/kotlin/it/unicam/cs/digital_library/email/model/client/Email.kt
+++ backend/src/main/kotlin/it/unicam/cs/digital_library/email/model/client/Email.kt
@@ -1,3 +1,3 @@
 package it.unicam.cs.digital_library.email.model.client
 
-data class Email(val to: String, val subject: String, val html: String? = null, val plain: String? = null)
+data class Email(val to: String, val subject: String, val html: String)
